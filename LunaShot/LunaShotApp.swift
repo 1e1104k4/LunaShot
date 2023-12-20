cef5115424dac3715981706c8008fabcc0367961
leila <143362178+1e1104k4@users.noreply.github.com>
2023-12-20 17:31:55 -0600

    Initial Commit

---
+++ LunaShot/LunaShotApp.swift
@@ -0,0 +1,17 @@
+//
+//  LunaShotApp.swift
+//  LunaShot
+//
+//  Created by Leila on 12/20/23.
+//
+
+import SwiftUI
+
+@main
+struct LunaShotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
