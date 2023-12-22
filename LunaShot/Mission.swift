6c218e4b5bd2b28076bcfc961778ebe39a887d50
leila <143362178+1e1104k4@users.noreply.github.com>
2023-12-22 15:29:08 -0600

    added - new file with struct for missions

---
+++ LunaShot/Mission.swift
@@ -0,0 +1,20 @@
+//
+//  Mission.swift
+//  LunaShot
+//
+//  Created by Leila on 12/22/23.
+//
+
+import Foundation
+
+struct Mission: Codable, Identifiable {
+    struct CrewRole: Codable {
+        let name: String
+        let role: String
+    }
+    
+    let id: Int
+    let launchDate: String?
+    let crew: [CrewRole]
+    let description: String
+}
