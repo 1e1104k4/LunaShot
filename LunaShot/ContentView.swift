ac2185f7a3e8c2ddfca2a2c7d818221be795200d
leila <143362178+1e1104k4@users.noreply.github.com>
2023-12-23 16:23:58 -0600

    fix: variable astronauts with type annotation

--- LunaShot/ContentView.swift
+++ LunaShot/ContentView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     
-    let astronauts = Bundle.main.decode("astronauts.json")
+    let astronauts: [String: Astronaut] = Bundle.main.decode("astronauts.json")
     
     var body: some View {
         Text("TEST")
