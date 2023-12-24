cd0dfc33a25a1a084d6daec4ac4b316c262ae46e
leila <143362178+1e1104k4@users.noreply.github.com>
2023-12-24 16:07:46 -0600

    prop to ContentView to define an adaptive column layout

--- LunaShot/ContentView.swift
+++ LunaShot/ContentView.swift
@@ -12,6 +12,10 @@ struct ContentView: View {
     let astronauts: [String: Astronaut] = Bundle.main.decode("astronauts.json")
     let missions: [Mission] = Bundle.main.decode("missions.json")
     
+    let columns = [
+        GridItem(.adaptive(minimum: 150))
+    ]
+    
     var body: some View {
         Text("TEST")
     }
