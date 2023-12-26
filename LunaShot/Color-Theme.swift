13e3b734aee07f2cb0cf8e20ee68bbe4674d349c
leila <143362178+1e1104k4@users.noreply.github.com>
2023-12-26 16:56:28 -0600

    dark blue design for app

---
+++ LunaShot/Color-Theme.swift
@@ -0,0 +1,17 @@
+//
+//  Color-Theme.swift
+//  LunaShot
+//
+//  Created by Leila on 12/26/23.
+//
+
+import SwiftUI
+
+extension ShapeStyle where Self == Color {
+    static var darkBackground: Color {
+        Color(red: 0.1, green: 0.1, blue: 0.2   )
+    }
+    static var lightBackground: Color {
+        Color(red: 0.2, green: 0.2, blue: 0.3)
+    }
+}
