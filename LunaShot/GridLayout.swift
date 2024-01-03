e2ea8287f1e12ec126ff80bf79c6007b610fabda
leila <143362178+1e1104k4@users.noreply.github.com>
2024-01-03 17:59:42 -0600

    2 new files for grid & list view

---
+++ LunaShot/GridLayout.swift
@@ -0,0 +1,18 @@
+//
+//  GridLayout.swift
+//  LunaShot
+//
+//  Created by Leila on 1/3/24.
+//
+
+import SwiftUI
+
+struct GridLayout: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GridLayout()
+}
---
+++ LunaShot/ListLayout.swift
@@ -0,0 +1,18 @@
+//
+//  ListLayout.swift
+//  LunaShot
+//
+//  Created by Leila on 1/3/24.
+//
+
+import SwiftUI
+
+struct ListLayout: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ListLayout()
+}
